5b8fa991c39508a469263db842c7cdc6d00d7923
Benjamin Spille <Benjamin Spille>
2017-03-12 22:05:31 -0400

    added work order creation function to the leader js

--- assets/javascript/leader.js
+++ assets/javascript/leader.js
@@ -1,29 +1,48 @@
-// on click id add-w-o 
-// create button with class accordion
-// append div with class panel
-// append to panel id issuer.val ref.val date.val
-// appen button to id w-o-list
+
 var leader = {
 	createWO: 	function() {
 					$('#add-w-o').click(function() {
+						// collect field inputs
 						var issuer = $('#issuer').val().trim();
 						var date = $('#date').val().trim();
 						var ref = $('#ref').val().trim();
 						var task = $('#w-o-task').val().trim();
-						var newBtn = $('<button class="accordion">' + 
+						// create button for accordion
+						var newBtn = $('<button data-toggle="collapse" class="accordion">' + 
 									ref + '</button>');
+						// assign href
+						newBtn.attr('href', '#' + ref);
+						// append button
 						$('#w-o-list').append(newBtn);
-						var newDiv = $('<div class="panel">');
-						newDiv.val(ref);
-						var newTag = $('<h3>Issuer: ' + issuer + '</h3>');
-						newDiv.append(newTag);
-						var newTag = $('<h3>Date: ' + date + '</h3>');
-						newDiv.append(newTag);
-						var newTag = $('<h3>Reference: ' + ref + '</h3>');
-						newDiv.append(newTag);
-						var newTag = $('<h3>task: ' +  + '</h3>');
-						newDiv.append(newTag);
+						// create accordion panel
+						var newDiv = $('<div class="panel-collapse collapse">');
+						// assign id for href above
+						newDiv.attr('id', ref);
+						// create table of information for work order
+						var newTable = $('<table>');
+						var newThead = $('<thead>');
+						var newTbody = $('<tbody>');
+						var newData = $('<tr><th><h3>Issuer: ' + issuer + '</h3></th></tr>');
+						newThead.append(newData);
+						var newData = $('<tr><th><h3>Date: ' + date + '</h3></th></tr>');
+						newThead.append(newData);
+						var newData = $('<tr><th><h3>Reference: ' + ref + '</h3></th></tr>');
+						newThead.append(newData);
+						var newData = $('<tr><td><h3>Task: ' + task + '</h3></td></tr>');
+						newTbody.append(newData);
+						// assemble table
+						newTable.append(newThead);
+						newTable.append(newTbody);
+						newDiv.append(newTable);
+						// append panel 
 						$('#w-o-list').append(newDiv);
+						// clear fields
+						$('#issuer').val('');
+						$('#date').val('');
+						$('#ref').val('');
+						$('#w-o-task').val('');
 					});
-	}
-};
+	},// end createWO
+	
+};//end leader object
+$('document').ready(leader.createWO);
